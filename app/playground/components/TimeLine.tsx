9bacfd5d07ca2e15bd4748fd49a17ef0eff43394
tchi-sama <tchisama41@gmail.com>
2023-11-03 13:47:03 -0700

    we will try d3 now

--- app/playground/components/TimeLine.tsx
+++ app/playground/components/TimeLine.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react'
 import {Tabs, Tab} from "@nextui-org/react";
+import TimelineCom from './global/Timeline';
 
 type Props = {}
 
@@ -11,6 +12,7 @@ function TimeLine({}: Props) {
             <Tabs variant='underlined' aria-label="Options">
                 <Tab key="timeline" title="Timeline"></Tab>
             </Tabs>
+            <TimelineCom duration={200} keyframes={[{time: 10, value: 0}, {time: 100, value: 100}]} />
         </div>  
     </div>
   )
--- app/playground/components/View.tsx
+++ app/playground/components/View.tsx
@@ -5,7 +5,10 @@ type Props = {}
 
 function View({}: Props) {
   return (
-    <div className='border-b flex-1 bg-foreground-100'>
+    <div className='border-b flex-1 bg-foreground-100 flex items-center justify-center'>
+        <div className='w-[500px] drop-shadow-sm flex items-center justify-center border bg-white h-[500px]'>
+            <Button>hello world</Button>
+        </div>
     </div>
   )
 }
