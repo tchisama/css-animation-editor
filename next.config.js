af8ede0d1dc8d6a7f019befc12ff2d89ae7314ad
tchi-sama <tchisama41@gmail.com>
2023-11-03 10:38:56 -0700

    try to add next ui

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const config = {
-    content: [
-      // ...
-      "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
-    ],
-    theme: {
-      extend: {},
-    },
-    darkMode: "class",
-    plugins: [nextui()]
-  }
-  
-  
+const nextConfig = {}
 
-module.exports = config
+module.exports = nextConfig
