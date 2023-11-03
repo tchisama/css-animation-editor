e43fd60475c747d289d9f809754c56840337ef36
tchi-sama <tchisama41@gmail.com>
2023-11-03 10:57:37 -0700

    add tabs to the controlers too

--- app/playground/components/Controlers.tsx
+++ app/playground/components/Controlers.tsx
@@ -1,10 +1,17 @@
+"use client"
+import { Tab, Tabs } from '@nextui-org/react'
 import React from 'react'
 
 type Props = {}
 
 function Controlers({}: Props) {
   return (
-    <div className='w-[500px] border'>Controlers</div>
+    <div className='w-[500px] border'>
+        <Tabs variant='underlined' aria-label="Options">
+            <Tab key="controlers" title="Controlers"></Tab>
+        </Tabs>
+    </div>
+
   )
 }
 
