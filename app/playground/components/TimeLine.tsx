8b215b045bb7c58e3e49822fadfcdaac6b993246
tchi-sama <tchisama41@gmail.com>
2023-11-03 17:30:10 -0700

    a

--- app/playground/components/TimeLine.tsx
+++ app/playground/components/TimeLine.tsx
@@ -1,21 +1,20 @@
 "use client"
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import {Tabs, Tab} from "@nextui-org/react";
-import TimelineCom from './global/Timeline';
 
 type Props = {}
 
-function TimeLine({}: Props) {
+
+function TimeLinee({}: Props) {
   return (
     <div className=' h-[300px]'>
-        <div className="flex w-full flex-col">
+        <div className="flex w-full h-full flex-col">
             <Tabs variant='underlined' aria-label="Options">
                 <Tab key="timeline" title="Timeline"></Tab>
             </Tabs>
-            <TimelineCom duration={200} keyframes={[{time: 10, value: 0}, {time: 100, value: 100}]} />
         </div>  
     </div>
   )
 }
 
-export default TimeLine
+export default TimeLinee
